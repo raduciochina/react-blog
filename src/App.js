2461675385c3f59c17116caf1498818140075f67
rciochin <ciochina7@gmail.com>
2023-05-11 12:49:23 +0300

    useState & useEffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import './App.css';
-import Navbar from './Navbar';
-import Home from './Home';
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-  
   return (
     <div className="App">
       <Navbar />
       <div className="content">
         <Home />
-      </div>  
+      </div>
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,35 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
-    return ( 
-        <div className="home">
-            <h2>Home Page</h2>
-        </div>
-     );
-}
- 
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
+  const [name, setName] = useState("mario");
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
+  useEffect(() => {
+    console.log("use effect ran");
+    console.log(name);
+  }, [name]);
+
+  return (
+    <div className="home">
+      <BlogList blogs={blogs} handleDelete={handleDelete} />
+      <button onClick={() => setName("luigi")}>change name</button>
+      <p>{name}</p>
+    </div>
+  );
+};
+
 export default Home;
