90a7c96df3919276476613455a00b1631491429e
rciochin <ciochina7@gmail.com>
2023-05-12 12:08:09 +0300

    add some routes, custom hook and abortController?

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-    return ( 
+  return (
     <nav className="navbar">
-        <h1>The Blog</h1>
-        <div className="links">
-            <a href="/">Home</a>
-            <a href="/create">New Post</a>
-        </div>
+      <h1>The Blog</h1>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/create">New Post</Link>
+      </div>
     </nav>
-     );
-}
- 
+  );
+};
+
 export default Navbar;
