fab8d67eb54074e9a59a0b22644d5df212a84d6f
rciochin <ciochina7@gmail.com>
2023-05-12 12:46:46 +0300

    add handleDelete

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,15 +1,24 @@
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const history = useHistory();
   const {
     data: blog,
     error,
     isPending,
   } = useFetch("http://localhost:8000/blogs/" + id);
+
+  const handleDelete = () => {
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
   return (
-    <div className="blog">
+    <div className="blog-details">
       {isPending && <div>Loading...</div>}
       {error && <div>{error}</div>}
       {blog && (
@@ -17,6 +26,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleDelete}>Delete</button>
         </article>
       )}
     </div>
